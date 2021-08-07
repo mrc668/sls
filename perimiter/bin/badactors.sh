b428f5092fa50a3fc0f206669658dfa26f1f9314
Milton Calnek <milton@calnek.com>
2021-08-06 23:30:14 -0600

    getting badactors out to the perimiter hosts.

--- perimiter/bin/badactors.sh
+++ perimiter/bin/badactors.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo ipset create newlist hash:net comment
-sudo ipset flush newlist
+ipset create newlist hash:net comment
+ipset flush newlist
 
 urls="
 http://rules.emergingthreats.net/blockrules/compromised-ips.txt
@@ -20,26 +20,35 @@ for u in  $urls ; do
 	echo Getting $u
 	echo curl -SskL --connect-timeout 3 $u
 	for i in $(curl -SskL --connect-timeout 3 $u | grep [0-9] | grep -Ev "[^0-9\.]" ); do
-		sudo ipset -exist add newlist $i comment "$u" || echo Failed on $i
+		ipset -q test whitelist $ip && {
+      echo $u tried to add $i from whitelist
+    } || {
+      ipset -exist add newlist $i comment "$u" || echo Failed on $i
+    }
 	done
-	sudo ipset list newlist | head | grep entries
+	ipset list newlist | head | grep entries
 	echo 
 done
 
 echo Getting http://www.dshield.org/ipsascii.html?limit=20000
 for i in $(curl -SsL http://www.dshield.org/ipsascii.html?limit=10000 | cut  -f1 | grep -Ev "[^0-9\.]" | sed -r "s/^0{1,2}//; s/\.0{1,2}/\./g; " ); do
-	sudo ipset -exist add newlist $i comment "dshield.org" || echo Failed on $i
+	ipset -exist add newlist $i comment "dshield.org" || echo Failed on $i
 done
-sudo ipset list newlist | head | grep entries
+ipset list newlist | head | grep entries
 echo 
 
 echo Getting https://isc.sans.edu/api/intelfeed
 for ip in $(curl -sS 'https://isc.sans.edu/api/intelfeed?json&requestor=milton@calnek.com' | jq -c '.[] | select ( .ip != null ) | { ip } ' | cut -d\" -f4); do
-	sudo ipset -q test isc-blocks $ip  || sudo ipset -exist add newlist $ip comment "isc intelfeed" || echo Failed on $ip
+	ipset -q test whitelist $ip || {
+    ipset -q test isc-blocks $ip  || {
+      ipset -exist add newlist $ip comment "isc intelfeed" || echo Failed on $ip
+    }
+  }
 done
-sudo ipset list newlist | head | grep entries
+ipset list newlist | head | grep entries
 
-sudo ipset swap newlist badactors
-sudo ipset destroy newlist
+ipset list | grep Name:.bad -q || ipset create badactors hash:net comment
+ipset swap newlist badactors
+ipset destroy newlist
 
 
