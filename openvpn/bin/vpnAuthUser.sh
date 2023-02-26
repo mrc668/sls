b067a6e86cfde8844533e84cbc78db5e5d1fc425
Milton Calnek <milton@calnek.com>
2023-02-26 14:06:04 -0600

    Locally managed user password file.

--- openvpn/bin/vpnAuthUser.sh
+++ openvpn/bin/vpnAuthUser.sh
@@ -3,30 +3,20 @@
 
 env | sort > /dev/shm/$(basename $0).env
 echo $* >  /dev/shm/$(basename $0).cli
+passwd="/etc/openvpn/server/vpnUsers"
+
 passedFile="$1"
+vpnUser=$(head -1 $passedFile)
+vpnPass=$(tail -1 $passedFile)
+enc="${vpnUser}:"$(echo $vpnPass | md5sum | cut -d" " -f1)
+logger -p daemon.info -t $0 $vpnUser $tls_id_0
 
-# prepare the password file
-sed -e 's/\(.*\)/\L\1/' -i $passedFile
-vpnuser=$(head -1 $passedFile )
-sed -e "1s/^/username = /; 2s/^/password = /;" -i $passedFile
-
-echo $vpnuser  $common_name  $trusted_ip $untrusted_ip $ifconfig_pool_local_ip | logger -p daemon.info -t openvpnAuth
-
-# Tie login to cert. login should be embedded iin cert.
-echo $common_name | grep $vpnuser -q || {
-  logger daemon.info -t openvpnAuth login name does not match common name on certificate
-  exit 2
+grep $enc $passwd -q && {
+  logger -p daemon.info -t $0 $vpnUser athenticated 
+  exit 0
 }
 
-# Not sure what this does
-#[ -f $CWD/authVars ] && . $PWD/authUser.vars
-
-# Not sure what this does
-#grep $vpnuser $PWD/userCerts | grep $common_name -q || exit 1
-
-smbclient -A $passedFile -L //192.168.0.6/ >/dev/null 2>&1 && exit 0
-
-cat $passedFile | logger -p daemon.info -t openvpnAuth
-
+logger -p daemon.info -t $0 $vpnUser failed login 
+logger -p daemon.info -t $0 $enc
 exit 1
 
