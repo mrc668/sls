3138d398ab070fb7b59c526e16a521686955d3c8
Milton Calnek <milton@calnek.com>
2025-12-31 18:06:23 -0600

    Dont touch the salt.repo anymore. Handle the copy via configuration instead.

--- salt/saltrepo.sh
+++ salt/saltrepo.sh
@@ -2,17 +2,10 @@
 # source: salt://sls/salt/saltrepo.sh
 
 doTheWork() {
-  # remove competitors
-  yum -y remove salt-py3-repo-3001-1.el7.noarch >/dev/null 2>&1
-  rm -f /etc/yum.repos.d/salt*
-
   # Get version number
   Major=0
   grep " 8" /etc/redhat-release -q && Major=8
   grep " 7" /etc/redhat-release -q && Major=7
-
-  # Get latestest repo
-  curl -sS https://repo.saltproject.io/py3/redhat/$Major/x86_64/latest.repo | sed -e "/failovermethod/d" > /etc/yum.repos.d/salt.repo
 
   # update
   yum -y update salt salt-minion salt-master > /var/log/yum-update-salt.log 2>&1
